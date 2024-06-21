9b068c3be6249d5bc33683263ae2d1a3b56e6dbf
Nio <nikris@mail.ru>
2024-06-21 20:22:31 +0300

    Refactoring in progress

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const PORT = 3000
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
-
+const notFound = require('./middleware/not_found')
 
 app.use(express.static('./public'))
 app.use(express.json())
 
 
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 const start = async () => {
   try {
